57b1edfed55d2e8a495ad328258ff394b89de032
maryam-fahmi <maryam.fahmi@torontomu.ca>
2023-09-04 14:16:47 -0400

    Updating Neaural Networks

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-N = 100;
+N = 500;
 const cars=generateCars(N);
 
 //bestCar updates on every frame, must use a let 
@@ -15,13 +15,21 @@ let bestCar=cars[0];
 
 //if best brain is found, you set that car to the bestCar value.
 if(localStorage.getItem("bestBrain")) {
-    bestCar.brain=JSON.parse(localStorage.getItem("bestBrain"));
+    for (let i=0; i < cars.length;i++) {
+        car[i].brain=JSON.parse(localStorage.getItem("bestBrain"));
+        if (i!=0) {
+            NeuralNetwork.mutate(cars[i].brain,0.1);
+        }
+    }
 }
 
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-800,30,50,"DUMMY",2),
 ];
 
 animate();
